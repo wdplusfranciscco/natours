abb0c6b6b44acaea602b3106738303f7b2f668af
wdplusfranciscco <wdplusfranciscco@gmail.com>
2020-08-30 21:36:01 -0500

    Add mockup delete handler

--- app.js
+++ app.js
@@ -89,7 +89,19 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
-app.delete();
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 < 0 || req.params.id * 1 > tours.length - 1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 
 const port = 3000;
 app.listen(port, () => {
