b5580f4c889e9ac5bfee12a76c4752dc6c4410ec
wdplusfranciscco <wdplusfranciscco@gmail.com>
2020-09-01 21:45:37 -0500

    Update time.js file with minor format changes made by the prettier extension

--- time.js
+++ time.js
@@ -1,18 +1,18 @@
 const logTimestamp = () => {
   var date = new Date();
 
-  var localDate = date.toLocaleDateString("es-ES", {
-    weekday: "short",
-    day: "numeric",
-    month: "short",
-    year: "numeric",
+  var localDate = date.toLocaleDateString('es-ES', {
+    weekday: 'short',
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
   });
 
-  var localTime = date.toLocaleTimeString("es-ES", {
+  var localTime = date.toLocaleTimeString('es-ES', {
     hour12: false,
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
   });
 
   console.log(`${localDate} - ${localTime}`);
