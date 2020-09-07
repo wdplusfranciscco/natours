8bb820836084da3143df0a85e90a476a986bc201
wdplusfranciscco <wdplusfranciscco@gmail.com>
2020-09-06 19:03:49 -0500

    Add checkID method to the tourController module

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,10 +3,7 @@ const tourController = require('./../controllers/tourController.js');
 
 const router = express.Router();
 
-router.param('id', (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
-  next();
-});
+router.param('id', tourController.checkID);
 
 router
   .route('/')
