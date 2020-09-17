2ce3ef6528f444107a0ff8c43f5abdf8eb730565
wdplusfranciscco <wdplusfranciscco@gmail.com>
2020-09-16 23:34:52 -0500

    Cleanup server.js code and point app back to remote db

--- server.js
+++ server.js
@@ -1,40 +1,26 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
-const logDebugStamp = require('./dev-utilities/log-debug-stamp');
 const logTimeStamp = require('./dev-utilities/log-time-stamp');
 
 dotenv.config({ path: './config.env' });
-
-logDebugStamp(
-  '20.09.16 [ server.js ] 19:39a',
-  `process.env.DATABASE:::   ${process.env.DATABASE}`
-);
-logDebugStamp(
-  '20.09.16 [ server.js ] 19:38b',
-  `process.env.DATABASE_PASSWORD:::   ${process.env.DATABASE_PASSWORD}`
-);
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
-logDebugStamp('20.09.16 [ server.js ] 19:38a', `DB:::   ${DB}`);
-
 mongoose
   // CONNECT TO LOCAL DATABASE WITH THIS:
-  .connect(process.env.DATABASE_LOCAL, {
-    //   <<< CONNECT TO REMOTE ATLAS DATABASE WITH THIS:
-    //.connect(DB, {
+  //     .connect(process.env.DATABASE_LOCAL, {
+  // CONNECT TO REMOTE ATLAS DATABASE WITH THIS:
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() =>
-    logDebugStamp('20.09.16 [ server.js ] 20:24a', 'DB Connection Successful!')
-  );
+  .then(() => logTimeStamp('server.js', 'DB Connection Successful!'));
 
 // console.log(process.env);
 
