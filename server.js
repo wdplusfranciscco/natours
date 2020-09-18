96f750ce0bd8a91f8e4d4577f7e86d75ae4ae93e
wdplusfranciscco <wdplusfranciscco@gmail.com>
2020-09-17 20:56:35 -0500

    Add mongoose schema and model for tours

--- server.js
+++ server.js
@@ -22,7 +22,23 @@ mongoose
   })
   .then(() => logTimeStamp('server.js', 'DB Connection Successful!'));
 
-// console.log(process.env);
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
