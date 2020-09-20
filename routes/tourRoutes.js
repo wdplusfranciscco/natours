b5270eab46a6618130efbc91a3b312418fdc44ba
wdplusfranciscco <wdplusfranciscco@gmail.com>
2020-09-20 07:10:12 -0500

    Refactor to get app closer to mvc architecture
    Remove or comment out functionality which used json data
    to simulate database during initial development.
    Add models folder and refactor by sending to it all
    model functionality initially coded in server.js

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const tourController = require('./../controllers/tourController.js');
+const tourController = require('../controllers/tourController.js');
 
 const router = express.Router();
-
-router.param('id', tourController.checkID);
 
 router
   .route('/')
