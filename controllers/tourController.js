1703829be562ed19d0f08e86fcf7ae587edf917e
wdplusfranciscco <wdplusfranciscco@gmail.com>
2020-12-30 08:57:29 -0600

    Add functionality to allow user to limit the number of fields that are returned by specifying them in the query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,6 +22,15 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.sort('-createdAt');
     }
+    
+    // 3) Field limiting
+    
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
 
     // Execute query
     const tours = await query;
